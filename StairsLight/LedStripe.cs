e49736efc638f36512d7b20ab507bd2d762c3c31
Maximilian Enderling <mitgliedschaftfo@gmx.de>
2018-12-23 11:56:13 +0100

    Urgh

--- StairsLight/LedStripe.cs
+++ StairsLight/LedStripe.cs
@@ -10,13 +10,14 @@ namespace StairsLight
 
         public LedStripe(int redIndex, int blueIndex, int greenIndex)
         {
-            RedController = PinController.GetGpioController(redIndex);
-            BlueController = PinController.GetGpioController(blueIndex);
+            //RedController = PinController.GetGpioController(redIndex);
+            //BlueController = PinController.GetGpioController(blueIndex);
             GreenController = PinController.GetGpioController(greenIndex);
         }
 
         public void SetColor(Color color)
         {
+            Console.WriteLine(color.G / 255f);
             GreenController.SetBrightness(color.G / 255f);
             //RedController.SetBrightness(color.R / 255f);
             //BlueController.SetBrightness(color.B / 255f);
