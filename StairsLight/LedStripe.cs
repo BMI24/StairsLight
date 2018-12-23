be8fccf52d72114c59be82bfb15be5162612d5a8
Maximilian Enderling <mitgliedschaftfo@gmx.de>
2018-12-23 11:52:55 +0100

    Only SetBrightness of GreenController

--- StairsLight/LedStripe.cs
+++ StairsLight/LedStripe.cs
@@ -18,8 +18,8 @@ namespace StairsLight
         public void SetColor(Color color)
         {
             GreenController.SetBrightness(color.G / 255f);
-            RedController.SetBrightness(color.R / 255f);
-            BlueController.SetBrightness(color.B / 255f);
+            //RedController.SetBrightness(color.R / 255f);
+            //BlueController.SetBrightness(color.B / 255f);
         }
     }
 }
